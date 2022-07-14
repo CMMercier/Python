116cc644257d5fb6e71ad0786a68188a35a97cac
CMMercier <43668197+CMMercier@users.noreply.github.com>
2022-07-14 18:42:25 -0400

    Update simple_server.py

--- simple_server.py
+++ simple_server.py
@@ -1,20 +1,25 @@
 import socket
+import threading
 
 SRV_ADDR = input("Type the server IP address: ")
 SRV_PORT = int(input("Type the server port: "))
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((SRV_ADDR, SRV_PORT))
-s.listen(1)
+def main()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((SRV_ADDR, SRV_PORT))
+    s.listen(5)
+    print(f'[*] Listening on {SRV_ADDR}:{SRV_PORT}')
+    
+    while True:
+        client, address = s.accept()
+        print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+        client_handler = threading.Thread(target=handle_client, args=(client,))
+        
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8)}')
+        sock.send(b'ACK')
 
-print("Server started! Waiting for connections...")
-connection, address = s.accept()
-print("Client connected with address: ", address)
-
-while 1:
-    data = connection.recv(1024)
-    if not data: break
-    connection.sendall(b'-- Message Received --\n')
-    print(data.decode('utf-8'))
-
-connection.close()
+if __name__ == '__main__':
+    main()
