4ad46889054e82ba39b087754ec18b3744db2795
CMMercier <43668197+CMMercier@users.noreply.github.com>
2022-07-14 18:48:03 -0400

    Update simple_server.py

--- simple_server.py
+++ simple_server.py
@@ -4,7 +4,7 @@ import threading
 SRV_ADDR = input("Type the server IP address: ")
 SRV_PORT = int(input("Type the server port: "))
 
-def main()
+def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((SRV_ADDR, SRV_PORT))
     s.listen(5)
@@ -18,7 +18,7 @@ def main()
 def handle_client(client_socket):
     with client_socket as sock:
         request = sock.recv(1024)
-        print(f'[*] Received: {request.decode("utf-8)}')
+        print(f'[*] Received: {request.decode("utf-8")}')
         sock.send(b'ACK')
 
 if __name__ == '__main__':
